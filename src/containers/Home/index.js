be9f95e8aa116a1ffc52e9afe4c46c04c63d5958
kartikpat <patpatiakartik@gmail.com>
2020-10-20 14:39:30 +0530

    conflicts resolved

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import SpaceXLaunches from "../../components/SpaceXLaunches";
 
 const Home = ({}) => (
+    <>
     <SpaceXLaunches></SpaceXLaunches>
+    <div>hello</div>
+    </>
 )
 
 export default Home;
